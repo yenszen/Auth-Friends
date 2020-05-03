0d2d15b2066755cbd092b449e2e84ec53be6e566
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-05-03 16:37:36 +0800

    built out FriendsList component

--- friends/src/components/App.js
+++ friends/src/components/App.js
@@ -1,15 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Login from "./Login";
 import FriendsList from "./FriendsList";
+import PrivateRoute from "./PrivateRoute";
 
 const App = () => {
   return (
     <Router>
       <div>
-        <Switch>
-          <Route exact path="/" component={Login} />
-        </Switch>
+        <nav
+          style={{
+            width: "30%",
+            display: "flex",
+            justifyContent: "space-between"
+          }}
+        >
+          <Link to="/">Login</Link>
+          <Link to="/protected">Friends List</Link>
+        </nav>
+        <Route exact path="/" component={Login} />
+        <PrivateRoute path="/protected" component={FriendsList} />
       </div>
     </Router>
   );
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,9 +1,73 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-class FriendsList extends React.Component {
-  render() {
-    return <div>FriendsList</div>;
-  }
-}
+const FriendsList = () => {
+  const [friends, setFriends] = useState([]);
+  const [newFriend, setNewFriend] = useState({ name: "", age: 0, email: "" });
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get("/friends")
+      .then(res => {
+        console.log("FriendsList: useEffect: GET", res);
+        setFriends(res.data);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  const onInputChange = e => {
+    setNewFriend({
+      ...newFriend,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const onFormSubmit = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("/friends", newFriend)
+      .then(res => {
+        console.log("FriendsList.js: onFormSubmit: success", res);
+        setFriends([...friends, res.data[res.data.length - 1]]);
+      })
+      .catch(err => console.log(err));
+  };
+
+  return (
+    <div>
+      <form onSubmit={onFormSubmit}>
+        <input
+          type="text"
+          name="name"
+          value={newFriend.name}
+          onChange={onInputChange}
+        />
+        <input
+          type="number"
+          name="age"
+          value={newFriend.age}
+          onChange={onInputChange}
+        />
+        <input
+          type="email"
+          name="email"
+          value={newFriend.email}
+          onChange={onInputChange}
+        />
+        <button type="submit">Add Friend</button>
+      </form>
+
+      {friends.map(friend => {
+        return (
+          <React.Fragment>
+            <h4>{friend.name}</h4>
+            <p>{friend.age}</p>
+            <p>{friend.email}</p>
+          </React.Fragment>
+        );
+      })}
+    </div>
+  );
+};
 
 export default FriendsList;
